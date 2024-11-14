b81aa555f3c1b84af6b92deee03ce3941f3387db
Omar Alterkait <omal8805@colorado.edu>
2024-11-15 07:59:27 +0900

    updated utils for saving and loading

--- tools/utils.py
+++ tools/utils.py
@@ -1,100 +1,171 @@
-
 import h5py
 import numpy as np
-import matplotlib.pyplot as plt
+import jax.numpy as jnp
 
-def load_data(filename):
+def full_to_sparse(charges, times):
+    """Convert full arrays to sparse representation by removing zero elements.
+
+    Parameters
+    ----------
+    charges : jnp.ndarray
+        Array of charge values for all detectors
+    times : jnp.ndarray
+        Array of time values for all detectors
+
+    Returns
+    -------
+    non_zero_indices : jnp.ndarray
+        Indices where charges are non-zero
+    non_zero_charges : jnp.ndarray
+        Charge values at non-zero locations
+    non_zero_times : jnp.ndarray
+        Time values at non-zero locations
+    """
+    non_zero_indices = jnp.nonzero(charges)[0]
+    non_zero_charges = charges[non_zero_indices]
+    non_zero_times = times[non_zero_indices]
+    return non_zero_indices, non_zero_charges, non_zero_times
+
+
+def sparse_to_full(sparse_indices, sparse_values, full_size):
+    """Convert sparse representation back to full array with zeros.
+
+    Parameters
+    ----------
+    sparse_indices : jnp.ndarray
+        Indices of non-zero elements
+    sparse_values : jnp.ndarray
+        Values at the non-zero indices
+    full_size : int
+        Size of the output array
+
+    Returns
+    -------
+    jnp.ndarray
+        Full array with sparse values inserted at specified indices
+    """
+    full_data = jnp.zeros(full_size)
+    return full_data.at[sparse_indices].set(sparse_values)
+
+
+def save_single_event(event_data, params, event_number=0, filename=None):
+    """Save single event simulation data to an HDF5 file in sparse format.
+
+    Parameters
+    ----------
+    event_data : tuple
+        (charges, average_times) arrays for the event
+    params : tuple
+        (cone_opening, track_origin, track_direction, initial_intensity)
+    event_number : int, optional
+        Event identifier number, defaults to 0
+    filename : str, optional
+        Custom path to output HDF5 file. If None, auto-generates name
+        in 'events' folder as 'event_X.h5' or 'event_X_TIMESTAMP.h5'
+
+    Returns
+    -------
+    str
+        Path to the saved file
+
+    Notes
+    -----
+    Saves data in a hierarchical structure with two groups:
+    - params: contains simulation parameters
+    - event: contains sparse event data (indices, charges, times)
+    """
+    charges, average_times = event_data
+    indices, sparse_charges, sparse_times = full_to_sparse(charges, average_times)
+
+    # Generate filename if not provided
+    if filename is None:
+        import os
+        from datetime import datetime
+
+        # Create events directory if it doesn't exist
+        os.makedirs('events', exist_ok=True)
+
+        base_filename = os.path.join('events', f'event_{event_number}.h5')
+
+        # If file exists, add timestamp
+        if os.path.exists(base_filename):
+            timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+            filename = os.path.join('events', f'event_{event_number}_{timestamp}.h5')
+        else:
+            filename = base_filename
+
+    with h5py.File(filename, 'w') as f:
+        # Save simulation parameters
+        params_group = f.create_group('params')
+        params_group.create_dataset('cone_opening', data=np.array(params[0]))
+        params_group.create_dataset('track_origin', data=np.array(params[1]))
+        params_group.create_dataset('track_direction', data=np.array(params[2]))
+        params_group.create_dataset('initial_intensity', data=np.array(params[3]))
+
+        # Save event data and number
+        event_group = f.create_group('event')
+        event_group.create_dataset('event_number', data=np.array(event_number))
+        event_group.create_dataset('indices', data=np.array(indices))
+        event_group.create_dataset('charges', data=np.array(sparse_charges))
+        event_group.create_dataset('times', data=np.array(sparse_times))
+
+    return filename
+
+
+def load_single_event(filename, num_detectors, sparse=True):
+    """Load single event simulation data from an HDF5 file.
+
+    Parameters
+    ----------
+    filename : str
+        Path to HDF5 file
+    num_detectors : int
+        Total number of detectors (needed for dense format)
+    sparse : bool, default=True
+        If True, returns data in sparse format
+        If False, converts to dense arrays
+
+    Returns
+    -------
+    params : tuple
+        (cone_opening, track_origin, track_direction, initial_intensity)
+    event_number : int
+        Event identifier number
+    If sparse=True:
+        indices : jnp.ndarray
+            Detector indices with non-zero values
+        charges : jnp.ndarray
+            Charge values at these indices
+        times : jnp.ndarray
+            Time values at these indices
+    If sparse=False:
+        charges : jnp.ndarray
+            Full array of charges for all detectors
+        times : jnp.ndarray
+            Full array of times for all detectors
+    """
     with h5py.File(filename, 'r') as f:
-        hit_pmt = np.array(f['hit_pmt'])
-        hit_charge = np.array(f['hit_charge'])
-        hit_time = np.array(f['hit_time'])
-        true_cone_opening = np.array(f['true_cone_opening'])[0]
-        true_track_origin = np.array(f['true_track_origin'])
-        true_track_direction = np.array(f['true_track_direction'])
+        # Load parameters
+        params_group = f['params']
+        cone_opening = float(params_group['cone_opening'][()])
+        track_origin = jnp.array(params_group['track_origin'][()])
+        track_direction = jnp.array(params_group['track_direction'][()])
+        initial_intensity = float(params_group['initial_intensity'][()])
 
-    return hit_pmt, hit_charge, hit_time, true_cone_opening, true_track_origin, true_track_direction
+        params = (cone_opening, track_origin, track_direction, initial_intensity)
 
-def relative_angle(vector1, vector2):
-    dot_product = np.dot(vector1, vector2)
-    magnitude_vector1 = np.linalg.norm(vector1)
-    magnitude_vector2 = np.linalg.norm(vector2)
-    cosine_angle = dot_product / (magnitude_vector1 * magnitude_vector2)
+        # Load event data
+        event_group = f['event']
+        event_number = int(event_group['event_number'][()])
+        indices = jnp.array(event_group['indices'][()])
+        charges = jnp.array(event_group['charges'][()])
+        times = jnp.array(event_group['times'][()])
 
-    angle_radians = np.arccos(cosine_angle)
-    angle_degrees = np.degrees(angle_radians)
+    if sparse:
+        return params, event_number, indices, charges, times
+    else:
+        # Convert sparse arrays to full dense arrays
+        dense_charges = sparse_to_full(indices, charges, num_detectors)
+        dense_times = sparse_to_full(indices, times, num_detectors)
 
-    return angle_degrees
-
-
-class Logger:
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.origins         = []
-        self.directions      = []
-        self.losses          = []
-        self.dir_err         = []
-        self.ori_err         = []
-        self.ch_angles       = []
-        self.ch_angles_err   = []
-
-    def add_data(self, origin, direction, true_dir, true_ori, ch_angle, true_ch_angle, loss):
-        self.origins.append(origin)
-        self.directions.append(direction)
-        self.losses.append(float(loss))
-        self.dir_err.append(relative_angle(true_dir, direction))
-        self.ori_err.append(np.linalg.norm(true_ori-origin))
-        self.ch_angles.append(ch_angle)
-        self.ch_angles_err.append(true_ch_angle-ch_angle)
-
-    def plot_angle_err(self):
-        plt.plot(range(len(self.dir_err[:])), self.dir_err[:], label='Direction angle error', color='darkorange')
-        plt.gca().set_xlabel('Iterations')
-        plt.gca().set_ylabel('Angle Error (degrees)')
-        plt.legend(frameon=False, loc='best')
-        plt.ylim(bottom=0.)
-
-    def plot_distance_err(self):
-        plt.plot(range(len(self.ori_err[:])), self.ori_err[:], label='Origin distance error', color='cornflowerblue')
-        plt.gca().set_xlabel('Iterations')
-        plt.gca().set_ylabel('Distance Error (meters)')
-        plt.legend(frameon=False, loc='best')
-        plt.ylim(bottom=0.)
-
-    def plot_ch_angle(self):
-        self.expected_cone_opening = 40
-        plt.plot(range(len(self.ch_angles[:])), self.ch_angles[:], color='hotpink')
-        plt.axhline(self.expected_cone_opening, color='darkgray', linestyle='--', label='expected')
-        plt.xlim(1, len(self.losses))
-        plt.ylim(bottom=min(self.expected_cone_opening, min(self.ch_angles[:])) / 1.43, top=max(self.expected_cone_opening, max(self.ch_angles[:])) * 1.3)
-        plt.gca().set_xlabel('Iterations')
-        plt.gca().set_ylabel('Cone Opening')
-        plt.legend(frameon=False, loc='best')
-
-    def plot_loss(self):
-        plt.plot(range(len(self.losses[:])), self.losses[:], color='k')
-        plt.gca().set_xlabel('Iterations')
-        plt.gca().set_ylabel('Loss')
-        plt.xlim(0, len(self.losses))
-        plt.yscale('log')
-
-    def plot_all(self):
-        plt.figure(figsize=(12, 9))
-
-        plt.subplot(2, 2, 1)
-        self.plot_angle_err()
-
-        plt.subplot(2, 2, 2)
-        self.plot_distance_err()
-
-        plt.subplot(2, 2, 3)
-        self.plot_ch_angle()
-
-        plt.subplot(2, 2, 4)
-        self.plot_loss()
-
-        plt.tight_layout()
-        plt.savefig('output_plots/optimization_results.pdf')
-        plt.show()
-
+        return params, event_number, dense_charges, dense_times
