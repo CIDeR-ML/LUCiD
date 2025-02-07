Added tests for SIREN. Fixed bug related photons origin calculation.

tools/generate.py
@@ -6,8 +6,8 @@ import h5py
 import numpy as np
 import jax.numpy as jnp
 import time
-from siren.siren import *
-from siren.table import *
+from tools.siren import *
+from tools.table import *
 
 def normalize(v, epsilon=1e-8):
     """Normalize a vector with numerical stability.
@@ -231,7 +231,7 @@ def new_differentiable_get_rays(track_origin, track_direction, energy, Nphot, ta
     
     # Convert ranges to meters and compute ray origins
     ranges = (smeared_trk * 300 + 300) / 100
-    ray_origins = jnp.ones((Nphot, 3)) * track_origin[None, :] + ranges[:, None] * track_direction[None, :]
+    ray_origins = jnp.ones((Nphot, 3)) * track_origin[None, :] + ranges[:, None] * normalize(track_direction[None, :])
     
     return ray_vectors, ray_origins, jnp.squeeze(new_photon_weights)
 

tools/simulation.py
@@ -5,8 +5,8 @@ from tools.geometry import generate_detector
 import jax
 import jax.numpy as jnp
 
-from siren.siren import *
-from siren.table import *
+from tools.siren import *
+from tools.table import *
 
 def setup_event_simulator(json_filename, n_photons=1_000_000, temperature=100):
     """
@@ -120,16 +120,6 @@ def create_event_simulator(propagate_photons, Nphot, NUM_DETECTORS, detector_poi
         detector_indices = prop_results['detector_indices']  # [max_detectors]
         hit_times = prop_results['times']  # [max_detectors, n_rays]
         hit_positions = prop_results['positions']  # [max_detectors, n_rays, 3]
-
-        # # Calculate charge deposits using competitive normalized weights
-        # photon_intensity = initial_intensity / Nphot
-        # flat_weights = weights.reshape(-1)
-        # flat_indices = detector_indices.reshape(-1)
-        # charges = jax.ops.segment_sum(
-        #     flat_weights * photon_intensity,
-        #     flat_indices,
-        #     num_segments=NUM_DETECTORS
-        # )
 
         # Expand photon_weights to match weights dimension
         expanded_photon_weights = jnp.broadcast_to(photon_weights, weights.shape)  # [max_detectors, n_rays]